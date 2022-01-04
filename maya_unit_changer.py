e4d20cbb45bb7400fc5398b38a0405183937d9a1
Kieron <17520284+KieronJenkins@users.noreply.github.com>
2022-01-04 16:34:28 +0000

    Updated maya_unit_changer.py
    Replaced print statement with warning

--- maya_unit_changer.py
+++ maya_unit_changer.py
@@ -1,4 +1,5 @@
 import maya.cmds as cmds
+import maya.OpenMaya as om
 
 # Made using Python. This script changes the units in Maya between MM, CM and M
 
@@ -6,7 +7,7 @@ def change_maya_units():
     # cmds.currentUnit(linear = "millimeter") # For each different Popup Menu Item, delete a different # before cmds.currentUnit on only one line
     # cmds.currentUnit(linear = "centimeter")
     # cmds.currentUnit(linear = "meter")
-    print("Units Changed") # Delete this if you don't want to print anything to console afterwards
+    om.MGlobal.displayWarning("Scale changed to MM") # Displays warning, replace MM to CM or M on each pop up
     
     
 change_maya_units()
